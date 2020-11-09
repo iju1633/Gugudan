46ac0ef44fbb7618a0d422c81962a6ba2606768b
82102 <82102@DESKTOP-FS1L2S6>
2020-11-09 18:35:47 +0900

    while과 for구문 사용하여 반복 없앰

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,23 +1,20 @@
 import java.util.Scanner;
 
-//사용자로부터 숫자를 받아서 해당 구구단을 출력시키는 클래스
+//사용자로부터 숫자를 받아서(6단,7단) 해당 구구단을 출력시키는 클래스
 public class Gugudan {
 	public static void main(String[] args) {
 
-		System.out.println("내가 출력할 구구단은?");
+		System.out.println("내가 출력할 구구단은?"); // 6단과 7단인걸 알지만;
 		Scanner scanner = new Scanner(System.in);
 		int num = scanner.nextInt();
-		
-		System.out.println(num * 1);
-		System.out.println(num * 2);
-		System.out.println(num * 3);
-		System.out.println(num * 4);
-		System.out.println(num * 5);
-		System.out.println(num * 6);
-		System.out.println(num * 7);
-		System.out.println(num * 8);
-		System.out.println(num * 9);
-		
+//		int i = 1;
+//		while(i<10) {
+//			System.out.println(num * i);
+//			i++;
+//		}
+		for(int i = 1; i<10; i++) {
+			System.out.println(num * i);
+		}
 		
 	}
 }
