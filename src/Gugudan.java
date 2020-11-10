3e067e53638382b02088578fad35622aa050b977
82102 <82102@DESKTOP-FS1L2S6>
2020-11-10 14:55:06 +0900

    입력값을 ,을 포함하여 2개를 받아서 구구단을 출력하는 함수

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,26 +1,31 @@
-//최종 요구사항 1
+//최종 요구사항 2
 //사용자가 입력한 값에 따라 크기가 다른 구구단을 계산해 출력한다.
-//예를 들어 사용자가 8을 입력하면 팔팔단, 19를 입력하면 십구십구단(2 * 1에서 19 * 19)을 계산해 출력한다.
-//이 요구사항은 앞의 실습을 소화했으면 구현할 수 있기 때문에 생략한다.
+//예를 들어 사용자가 "8,7"과 같은 문자열을 입력하면 팔칠단을 구현한다.
+//팔칠단은 2 * 1 ... 2 * 7, 3 * 1 ... 3 * 7, ... , 8 * 1 ... 8 * 7 까지 구현하는 것을 의미한다.
 
 import java.util.Scanner;
 
-public class Gugudan {
+public class Gugudan{
 	
 	public static void main(String[] args) {
 		
-		System.out.println("what is your number for Gugudan?");
+		System.out.println("What is your number?");
 		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
-		System.out.println("Your chosen number : "+num);
+		String inputValue = sc.nextLine(); // int num = sc.nextInt();과 다르게 ,이 포함된 입력값이기때문에 String!
 		
-		int[] result = new int[num];
+		String[] splitedValue = inputValue.split(",");
 		
-		for(int i=0; i<result.length; i++) {
-			result[i] = (1+i) * num;
-			System.out.println(result[i]);
+		int first = Integer.parseInt(splitedValue[0]);
+		int second = Integer.parseInt(splitedValue[1]);
+		
+		int[] result = new int[first * second];
+		
+		for(int j=1; j<first+1; j++) {
+		for(int i=1; i<second+1;i++) {
+			result[i-1] = i * j;
+			System.out.println(j+"*"+i+" = " + result[i-1]);
 		}
-		
+		}
 		
 	}
 }
