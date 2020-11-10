6661fd3fbb2084deb32189b184951d2905c1261c
82102 <82102@DESKTOP-FS1L2S6>
2020-11-10 14:02:41 +0900

    scanner함수를 통해 원하는 단을 입력받아서 해당 구구단을 출력시키는 함수

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,21 +1,33 @@
+import java.util.Scanner; // Scanner 대문자
 
-//이중 for문을 이용한 구구단 출력
+//method 함수를 설정하여 좀더 이해가 쉽게 코드 짜기
 public class Gugudan {
-
-
+	public static int[] calculate(int times) { //배열함수 설정.
+		int[] result = new int[9]; // new 까먹지마
+		
+		for(int i=0; i<result.length; i++) {
+			result[i] = (1+i) * times; // times를 왜 설정해놓고 안써
+		}
+		
+		return result;
+	}
+	public static void print(int[] result) {
+		for(int i=0; i<result.length;i++) {
+			System.out.println(result[i]);// 그냥 result라고 하니까 [I@8efb846와 같은 결과 나옴.
+		}
+	}
+	
+	
 	public static void main(String[] args) {
+		System.out.println("What is your number?");
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt(); // 기억 안났음
+		System.out.println("your adopted num : "+num);
 		
-		int[] result = new int[9]; //크기가 9인 배열함수 설정
+			int result[] = calculate(num);
+			print(result);
+			
 		
-		for(int j = 1; j<10; j++)
-		{
-			System.out.println(j+"단입니다");
-		for(int i = 0; i<9; i++)
-		{
-			result[i] = j * (i+1);
-			System.out.println(result[i]);
-		}
-		
-		}
+
 	}
 }
